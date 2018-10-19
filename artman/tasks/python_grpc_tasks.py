eb646838f5f5917cac2f13efb676034beb753aa7
Hanzhen Yi <hzyi@google.com>
2018-10-19 11:04:25 -0700

    should work

--- artman/tasks/python_grpc_tasks.py
+++ artman/tasks/python_grpc_tasks.py
@@ -201,7 +201,7 @@ class PythonMoveProtosTask(task_base.TaskBase):
         )
 
         # Move the contents into the GAPIC directory.
-        self.exec_command(['mv', os.path.join(src, 'proto'), target])
+        self.exec_command(['mv', src, os.path.join(target, 'proto')])
 
         # Create an __init__.py file in the proto directory.
         # This is necessary for Python 2.7 compatibility.
